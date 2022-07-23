55f413277d579393125e1cdde2bc6591195d8d20
Informática CNBA <105891917+INFOCNBA@users.noreply.github.com>
2022-07-23 13:08:28 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
 function alarma () {
     music.playMelody("C5 A B G A F G E ", 500)
 }
+input.onButtonPressed(Button.A, function () {
+    tiempoInicial += 1
+    tiempoActual = tiempoInicial
+})
 function decrementarTiempoActual () {
     tiempoActual += -1
     basic.showNumber(tiempoActual)
 }
+input.onButtonPressed(Button.B, function () {
+    tiempoInicial += -1
+    tiempoActual = tiempoInicial
+})
 let tiempoActual = 0
-let tiempoInicial = 5
+let tiempoInicial = 0
+tiempoInicial = 5
 tiempoActual = tiempoInicial
 loops.everyInterval(1000, function () {
     if (tiempoActual > 0) {
